c81a4703156e3f8c7ae8aa66a70b4583e9befe0e
unknown <mcvilleanrapsody@gmail.com>
2020-03-15 17:17:32 +0800

    express setup with initial routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'Hello World , this is my new app' });
+});
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`server started in port ${PORT}`);
+});
